916a6c56acc2838ecebe6d59bdb1ebf8b714a07b
Ehsan Varasteh <ehsan.ecc@gmail.com>
2023-01-13 16:06:32 +0330

    upload files

---
+++ aria2c_broken_download.py
@@ -0,0 +1,73 @@
+#!/bin/python3
+
+import os, sys
+import argparse
+from math import ceil, floor
+from struct import pack, unpack
+
+# Control file (*.aria2) format
+#  0                   1                   2                   3
+#  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+# +---+-------+-------+-------------------------------------------+
+# |VER|  EXT  |INFO   |INFO HASH ...                              |
+# |(2)|  (4)  |HASH   | (INFO HASH LENGTH)                        |
+# |   |       |LENGTH |                                           |
+# |   |       |  (4)  |                                           |
+# +---+---+---+-------+---+---------------+-------+---------------+
+# |PIECE  |TOTAL LENGTH   |UPLOAD LENGTH  |BIT-   |BITFIELD ...   |
+# |LENGTH |     (8)       |     (8)       |FIELD  | (BITFIELD     |
+# |  (4)  |               |               |LENGTH |  LENGTH)      |
+# |       |               |               |  (4)  |               |
+# +-------+-------+-------+-------+-------+-------+---------------+
+# |NUM    |INDEX  |LENGTH |PIECE  |PIECE BITFIELD ...             |
+# |IN-    |  (4)  |  (4)  |BIT-   | (PIECE BITFIELD LENGTH)       |
+# |FLIGHT |       |       |FIELD  |                               |
+# |PIECE  |       |       |LENGTH |                               |
+# |  (4)  |       |       |  (4)  |                               |
+# +-------+-------+-------+-------+-------------------------------+
+
+#         ^                                                       ^
+#         |                                                       |
+#         +-------------------------------------------------------+
+#                 Repeated in (NUM IN-FLIGHT) PIECE times
+
+MiB = 1048576
+
+a = argparse.ArgumentParser("aria2c HTTP(S)/FTP Broken Download Repair", description="Not repair, but find not downloaded parts (guessing) and (re)build <filename>.aria2 file again :)")
+a.add_argument(metavar='files', dest='files', nargs='+', type=str, help="Broken downloaded file(s)")
+args = a.parse_args()
+
+def create_control_file(file_address, bitfield):
+    with open(file_address + '.aria2', "wb") as fo:
+        INFO_HASH_LENGTH=0
+        EXT=0
+        PIECE_LENGTH=MiB
+        TOTAL_LENGTH=os.path.getsize(file_address)
+        UPLOAD_LENGTH=0
+        bitfield_length = ceil(len(bitfield)/8)
+        bitfield_packed = [0] * ceil(len(bitfield)/8)
+        for i in range(len(bitfield)):
+            if bitfield[i] == '1':
+                bitfield_packed[int(i/8)] |= 1 << (7-(i%8))
+        fo.write(pack('>HIIIQQI', 1, EXT, INFO_HASH_LENGTH, PIECE_LENGTH, TOTAL_LENGTH, UPLOAD_LENGTH, bitfield_length))
+        fo.write(bytes(bitfield_packed))
+        NUM_INFLIGHT_PIECES=0
+        fo.write(pack('>I', NUM_INFLIGHT_PIECES))
+
+for f in args.files:
+    if not os.path.exists:
+        print(f'ERROR: file "{f}" not exists.')
+        continue
+    fsize = os.path.getsize(f)
+    bitfield = ['0'] * ceil(fsize/MiB)
+    with open(f, "rb") as fi:
+        for i in range(ceil(fsize/MiB)):
+            print(f'\r{f} [{i:04d}/{ceil(fsize/MiB):04d}]', end='')
+            fi.seek(i * MiB, os.SEEK_SET) # next block
+            if fi.read(1024) == (b'\x00' * 1024):
+                print(' EMPTY')
+                if i > 0:
+                    bitfield[i-1] = '0'
+            else:
+                bitfield[i] = '1'
+        create_control_file(f, bitfield)
---
+++ re-run.py
@@ -0,0 +1,50 @@
+import json, urllib3
+from time import sleep
+
+class aria2rpc():
+    secret = None
+    server_ip = None
+    def __init__(self, server_ip:str, secret:str=None) -> None:
+        self.server_ip = server_ip
+        self.secret = secret
+
+    def __call__(self, *args: str, **kwds: str):
+        params = [f'token:{self.secret}'] if self.secret else []
+        params += args[1:]
+        jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer',
+                      'method':'aria2.'+args[0],
+                      'params':params})
+        http = urllib3.PoolManager()
+        r = http.request('POST', f'http://{self.server_ip}/jsonrpc', body=jsonreq, headers={'Content-Type': 'application/json'})
+
+        rr = json.loads(r.data)
+        if r.status == 200:
+            return rr['result']
+        else:
+            print(f"ERROR{rr['error']['code']}: {rr['error']['message']}")
+            return None
+
+aria2caller = aria2rpc('192.168.0.13:6800', 'pero')
+
+while True:
+    downloadState = len(aria2caller('tellActive')) == 0
+    stoppedOnes = aria2caller('tellStopped', 0, 1000)
+    if stoppedOnes:
+        for stoppedOne in stoppedOnes:
+            files = aria2caller('getFiles', stoppedOne['gid'])
+            aria2caller('removeDownloadResult', stoppedOne['gid'])
+            aria2caller('addUri', [stoppedOne['files'][0]['uris'][0]['uri']], {"always-resume":"true", "continue":"true", "dir":stoppedOne['dir'], "pause":"true" if downloadState == True else "false"})
+    
+    sleep(60)
+
+# scenario
+# we get stopped list
+# we get all info about stopped ones (url, options)
+# we delete stop ones and start them again with gathered options! (restart)
+# *** UNTIL dawn! 7am
+
+# 1. getFiles (gid)
+# 2. removeDownloadResult (gid) < ['OK']
+# 3. addUri (URI, allow-overwrite:false, allow-piece-length-change:false,
+#   always-resume:true, async-dns:true, auto-file-renaming:true, continue:true,
+#   dir:"...")
